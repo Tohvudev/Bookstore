dd24fc12f5f4ab6ce86b558420ea8a6099d8562e
Christoph <109544835+Tohvudev@users.noreply.github.com>
2024-02-26 13:29:11 +0200

    CRUD add  delete

--- src/main/java/backendk24/bookstore/web/BookController.java
+++ src/main/java/backendk24/bookstore/web/BookController.java
@@ -1,8 +1,10 @@
 package backendk24.bookstore.web;
 
+import backendk24.bookstore.domain.Book;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import backendk24.bookstore.domain.BookRepository;
@@ -12,17 +14,32 @@ import backendk24.bookstore.web.BookController;
 public class BookController {
 
     @Autowired
-    private BookRepository bookRepository;
+    BookRepository bookRepository;
 
-    @RequestMapping(value = "/index", method = RequestMethod.GET)
-    public String getBook(Model model) {
-        return "welcomepage";
-    }
+
 
     @RequestMapping(value = "/booklist", method = RequestMethod.GET)
     public String listBooks(Model model) {
         model.addAttribute("books", bookRepository.findAll());
         return "booklist";
+    }
+
+    @RequestMapping(value = "/add")
+    public String addBook(Model model) {
+        model.addAttribute("book", new Book());
+        return ("addbook");
+    };
+
+    @RequestMapping(value = "/save", method = RequestMethod.POST)
+    public String save(Book book) {
+        bookRepository.save(book);
+        return "redirect:booklist";
+    }
+
+    @RequestMapping(value="/delete/{id}", method = RequestMethod.GET)
+    public String deleteBook(@PathVariable("id") Long bookId, Model model) {
+        bookRepository.deleteById(bookId);
+        return "redirect:../booklist";
     }
 
 }
