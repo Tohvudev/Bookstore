93e9206324495355bc4aeaafdf2eb9d4ffd70433
Christoph <109544835+Tohvudev@users.noreply.github.com>
2024-03-25 13:03:09 +0200

    added user DB with login

--- src/main/java/backendk24/bookstore/BookstoreApplication.java
+++ src/main/java/backendk24/bookstore/BookstoreApplication.java
@@ -1,16 +1,14 @@
 package backendk24.bookstore;
 
-import backendk24.bookstore.domain.Category;
-import backendk24.bookstore.domain.CategoryRepository;
+import backendk24.bookstore.domain.*;
+import backendk24.bookstore.domain.UserRepository;
+import backendk24.bookstore.domain.User;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-
-import backendk24.bookstore.domain.Book;
-import backendk24.bookstore.domain.BookRepository;
 
 @SpringBootApplication
 public class BookstoreApplication {
@@ -20,8 +18,9 @@ public class BookstoreApplication {
 		SpringApplication.run(BookstoreApplication.class, args);
 	}
 
+
 	@Bean
-	public CommandLineRunner bookRunner(BookRepository bookRepository, CategoryRepository categoryRepository) {
+	public CommandLineRunner bookRunner(BookRepository bookRepository, CategoryRepository categoryRepository, UserRepository userRepository) {
 		return (args) -> {
 			log.info("save");
 
@@ -49,7 +48,17 @@ public class BookstoreApplication {
 			book3.setCategory(category4);
 			bookRepository.save(book3);
 
+			// TESTIKÄYTTÄJÄT DATABASEEN MUISTA MYÖHEMMIN POISTAA SALASANAT
+
+
+			User user1 = new User("user", "$2a$12$iWojeNpwIkHYmTLM/H2ySeGwjiTfuNHgcjtQ1jAcomtAwkNGQUCli", "user@book.com", "USER"); // pw user
+			User user2 = new User("admin", "$2a$12$1SPIDgsGyFsc5/hkVFUxmec2Onm6L65l6POlq8Wkc1aX7pivjMYPi", "admin@book.com", "ADMIN"); // pw admin
+			User user3 = new User("testeri", "$2a$12$geEmOFpD7zndA.GLsuPxlej3edBFA.K3mxONt2.6dD8oMm5iYMbV6", "testeri@book.com", "ADMIN"); // pw on testi
+			userRepository.save(user1);
+			userRepository.save(user2);
+			userRepository.save(user3);
+
 			log.info("fetch");
 		};
-	}
-}
+
+	}}
